5f269323d78a43247014fe73ef0abbe50133859c
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-11-12 09:53:04 -0500

    ADD WEATHER

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,14 @@ public class Main {
         System.out.println("Hello World");
     }
 
+    public static void weather(){
+        System.out.println("snoww");
+    }
+
 
 
     public static void main(String[] args) {
         helloworld();
+        weather();
     }
 }
